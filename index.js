a17dc81eff081b98e94d5b65c5500305950c2a71
diaaanek <dkorongy@gmail.com>
2019-01-29 11:36:53 -0500

    Done.

--- index.js
+++ index.js
@@ -11,14 +11,31 @@ return document.querySelector("#nested .target")
 }
 //
 // Returns the most deeply nested child in #grand-node:
-function deepestChild(){
-  const = querySelectorAll()
+function deepestChild() {
+  let node = document.getElementById('grand-node');
+  let nextNode = node.children[0];
+
+  while (nextNode) {
+    node = nextNode;
+    nextNode = node.children[0];
+  }
+
+  return node;
 }
+
 
 
 //
 // Define a function increaseRankBy(n) that increases the ranks in all of the .ranked-lists by n. (You might need to make use of parseInt()
 
-function deepestChild() {
-  return document.querySelector('#grand-node div div div div')
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list');
+
+  for (let i = 0, l = rankedLists.length; i < l; i++) {
+    let children = rankedLists[i].children;
+
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n;
+    }
+  }
 }
