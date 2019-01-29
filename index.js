f116dc562de0b4aed194e9980f04d14d2e160e3f
diaaanek <dkorongy@gmail.com>
2019-01-28 22:07:55 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 
+ // Document.querySelector()
+ // returns the first element that matches the selector:
+function getFirstSelector(selector){
+    return document.querySelector(selector)
+// return selector
+}
+//
+function nestedTarget(id){
+return document.querySelector("#nested .target")
+}
+//
+// Returns the most deeply nested child in #grand-node:
+function deepestChild(){
+  const = querySelectorAll()
+}
+
+
+//
+// Define a function increaseRankBy(n) that increases the ranks in all of the .ranked-lists by n. (You might need to make use of parseInt()
+
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div')
+}
